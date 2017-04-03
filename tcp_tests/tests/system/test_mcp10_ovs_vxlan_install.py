055b5068664d0f64cacc28578642d0e4c81fea07
Tatyana Leontovich <tleontovich@mirantis.com>
2017-04-03 13:17:25 +0300

    Add initial commit with deploy mcp10_ovs

---
+++ tcp_tests/tests/system/test_mcp10_ovs_vxlan_install.py
@@ -0,0 +1,102 @@
+#    Copyright 2017 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+import copy
+import time
+
+import pytest
+
+from tcp_tests import settings
+from tcp_tests.helpers import ext
+from tcp_tests import logger
+
+LOG = logger.logger
+
+
+@pytest.mark.deploy
+class TestMCP10OvsVxlanInstall(object):
+    """Test class for testing mcp10 vxlan deploy"""
+
+    #salt_cmd = 'salt -l debug '  # For debug output
+    #salt_call_cmd = 'salt-call -l debug '  # For debug output
+    salt_cmd = 'salt --hard-crash --state-output=mixed --state-verbose=False '  # For cause only output
+    salt_call_cmd = 'salt-call --hard-crash --state-output=mixed --state-verbose=False '  # For cause only output
+    #salt_cmd = 'salt --state-output=terse --state-verbose=False '  # For reduced output
+    #salt_call_cmd = 'salt-call --state-output=terse --state-verbose=False '  # For reduced output
+
+    # @pytest.mark.snapshot_needed
+    # @pytest.mark.fail_snapshot
+    def test_mcp10_ovs_vxlan_install(self, underlay, openstack_deployed,
+                                     show_step, rally):
+        """Test for deploying an mcp environment and check it
+
+        Scenario:
+            1. Prepare salt on hosts
+            2. Setup controller nodes
+            3. Setup compute nodes
+
+        """
+        LOG.info("*************** DONE **************")
+
+    def test_mcp10_ovs_vxlan_install_run_rally(self, underlay,
+                                               openstack_deployed,
+                                               show_step, rally):
+        """Test for deploying an mcp environment and check it
+
+        Scenario:
+            1. Prepare salt on hosts
+            2. Setup controller nodes
+            3. Setup compute nodes
+            4. Run rally
+
+        """
+        # prepare rally
+        rally.prepare()
+        rally.pull_image()
+        rally.run()
+        # run tempest
+        rally.run_tempest()
+
+        res = rally.get_results()
+
+        fail_msg = 'Tempest verification fails {}'.format(res)
+        assert res['failures'] == 0, fail_msg
+
+    # @pytest.mark.snapshot_needed
+    # @pytest.mark.fail_snapshot
+    def test_mcp10_vxlan_install_with_scripts(self, config, underlay,
+                                              salt_deployed,
+                                              show_step, rally):
+        """Test for deploying an mcp environment with scripts and check it
+
+        Scenario:
+            1. Prepare salt on hosts
+            2. Setup controller nodes
+            3. Setup compute nodes
+
+        """
+
+        cmd = 'cd /srv/salt/reclass/scripts/; ./bootstrap_all.sh'
+        underlay.check_call(cmd, host=config.salt.salt_master_host, verbose=True)
+
+        # prepare rally
+        rally.prepare()
+        rally.pull_image()
+        rally.run()
+        # run tempest
+        rally.run_tempest()
+
+        res = rally.get_results()
+
+        fail_msg = 'Tempest verification fails {}'.format(res)
+        assert res['failures'] == 0, fail_msg
