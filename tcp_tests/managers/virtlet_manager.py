b1b0543a28cc6de5d50443d0b46fd1c992d6ca16
Victor Ryzhenkin <vryzhenkin@mirantis.com>
2017-06-13 12:51:39 +0400

    [WIP] Spawn VM with specific resource quotas
    - Spawn VM and verify that provided resources is correct
    on VM. Delete VM after interaction.
    
    Change-Id: Iaba6d3379e85863b22bf06a23204377a98c1e2ac

--- tcp_tests/managers/virtlet_manager.py
+++ tcp_tests/managers/virtlet_manager.py
@@ -32,3 +32,43 @@ class VirtletManager(ExecuteCommandsMixin):
         self.execute_commands(commands,
                               label='Install Virtlet project')
         self.__config.virtlet.virtlet_installed = True
+
+    def get_virtlet_node(self):
+        # Get any controller
+        cmd = "salt 'ctl01*' grains.get fqdn|tail -n1"
+        result = self.__underlay.check_call(
+            cmd, host=self.__config.salt.salt_master_host)
+
+        ctl01_name = result['stdout'].strip()
+
+        # Get virtlet node hostname
+        cmd = "kubectl get po -n kube-system -o=wide | grep virtlet | " \
+              "awk {'print $7'}"
+        result = self.__underlay.check_call(cmd, node_name=ctl01_name)
+        return result['stdout'].strip()
+
+    def adjust_cirros_resources(self, cpu=2, memory='256Mi'):
+        virtlet_node = self.get_virtlet_node()
+        # We will need to change params in case of example change
+        cmd = "cd ~/virtlet/examples && " \
+              "cp cirros-vm.yaml cirros-vm-exp.yaml && " \
+              "sed -r 's/^(\s*)(VirtletVCPUCount\s*:\s*\"1\"\s*$)/" \
+              "\1VirtletVCPUCount: \"{0}\"/' cirros-vm-exp.yaml && " \
+              "sed -r 's/^(\s*)(memory\s*:\s*128Mi\s*$)/\1memory: " \
+              "{1}/' cirros-vm-exp.yaml".format(cpu, memory)
+        self.__underlay.check_call(cmd, node_name=virtlet_node)
+
+    def get_domain_name(self):
+        pass
+
+    def get_vm_cpu_count(self, domain_id):
+        pass
+
+    def get_vm_memory_count(self, domain_id):
+        pass
+
+    def start_vm(self):
+        pass
+
+    def delete_vm(self):
+        pass
