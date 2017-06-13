95897a75db515279bdbc9551cd0ec4abb7461562
Victor Ryzhenkin <vryzhenkin@mirantis.com>
2017-06-13 12:51:39 +0400

    [WIP] Spawn VM with specific resource quotas
    - Spawn VM and verify that provided resources is correct
    on VM. Delete VM after interaction.
    
    Change-Id: Iaba6d3379e85863b22bf06a23204377a98c1e2ac

--- tcp_tests/managers/virtlet_manager.py
+++ tcp_tests/managers/virtlet_manager.py
@@ -32,3 +32,61 @@ class VirtletManager(ExecuteCommandsMixin):
         self.execute_commands(commands,
                               label='Install Virtlet project')
         self.__config.virtlet.virtlet_installed = True
+
+    def get_virtlet_node(self):
+        # Get any controller
+        cmd = "salt 'ctl01*' grains.get fqdn|tail -n1"
+        result = self.__underlay.check_call(
+            cmd, host=self.__config.salt.salt_master_host)
+
+        ctl01_name = result['stdout'].strip()
+
+        # Get virtlet node hostname
+        cmd = "kubectl get po -n kube-system -o=wide | grep virtlet | " \
+              "awk {'print $7'}"
+        result = self.__underlay.check_call(cmd, node_name=ctl01_name)
+        return result['stdout'].strip()
+
+    def adjust_cirros_resources(self, cpu=2, memory='256',
+                                target_yaml='cirros-vm-exp.yaml'):
+        virtlet_node = self.get_virtlet_node()
+        # We will need to change params in case of example change
+        cmd = "cd ~/virtlet/examples && " \
+              "cp cirros-vm.yaml {2} && " \
+              "sed -r 's/^(\s*)(VirtletVCPUCount\s*:\s*\"1\"\s*$)/" \
+              "\1VirtletVCPUCount: \"{0}\"/' {2} && " \
+              "sed -r 's/^(\s*)(memory\s*:\s*128Mi\s*$)/\1memory: " \
+              "{1}Mi/' {2}".format(cpu, memory, target_yaml)
+        self.__underlay.check_call(cmd, node_name=virtlet_node)
+
+    def get_domain_name(self):
+        virtlet_node = self.get_virtlet_node()
+        cmd = "cd ~/virtlet/examples && " \
+              "./virsh.sh list | grep -i 'cirros' | awk {'print $2'}"
+        result = self.__underlay.check_call(cmd, node_name=virtlet_node)
+        return result['stdout'].strip()
+
+    def get_vm_cpu_count(self, domain_id):
+        virtlet_node = self.get_virtlet_node()
+        cmd = "cd ~/virtlet/examples && " \
+              "./virsh.sh dumpxml {0} | " \
+              "grep 'cpu' | grep -o '[[:digit:]]*'".format(domain_id)
+        result = self.__underlay.check_call(cmd, node_name=virtlet_node)
+        return int(result['stdout'].strip())
+
+    def get_vm_memory_count(self, domain_id):
+        virtlet_node = self.get_virtlet_node()
+        cmd = "cd ~/virtlet/examples && " \
+              "./virsh.sh dumpxml {0} | " \
+              "grep 'memory unit' | grep -o '[[:digit:]]*'".format(domain_id)
+        result = self.__underlay.check_call(cmd, node_name=virtlet_node)
+        return int(result['stdout'].strip())
+
+    def start_vm(self, target_yaml):
+        pass
+
+    def wait_for_vm_pod_status(self, status):
+        pass
+
+    def delete_vm(self, target_yaml):
+        pass
