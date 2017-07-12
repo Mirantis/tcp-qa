7c7ed43531ed35b4e93aefe6d4b7a4fbf2096964
Dennis Dmitriev <ddmitriev@mirantis.com>
2017-07-12 16:36:41 +0300

    Add deployment steps for model virtual-mcp-ocata-cicd
    Change-Id: I55a62a827a9d209821cf69f82b8b0eda58d816f4

---
+++ tcp_tests/tests/system/test_oss_install.py
@@ -0,0 +1,35 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import pytest
+
+from tcp_tests import logger
+
+LOG = logger.logger
+
+
+class TestOSSInstaller(object):
+    """Test class for testing Operational Support System Tools deployment"""
+
+    @pytest.mark.fail_snapshot
+    def test_oss_install_default(self, underlay, oss_deployed,
+                                 show_step):
+        """Test for deploying an OSS environment and check it
+
+        Scenario:
+            1. Prepare salt on hosts
+            2. Setup cid* nodes
+
+        """
+        LOG.info("*************** DONE **************")
