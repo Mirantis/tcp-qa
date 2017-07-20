9782ceb1afac2193a6f503a16ecda85c3b3d0ec6
valentyn.yakovlev <valentyn.yakovlev@gmail.com>
2017-07-20 07:44:43 -0400

    Revert "enabling test 'test_calico_network_policies'"
    This reverts commit 44c8bcd7d17c1b3074bfa4130213fe8f4b0bc4b2.
    
    Change-Id: Ifb667171b6500b73d695dda0efd7f0f27125313b

--- tcp_tests/tests/system/test_calico.py
+++ tcp_tests/tests/system/test_calico.py
@@ -148,7 +148,9 @@ class TestMCPCalico(object):
         netchecker.wait_check_network(k8sclient, works=True)
 
     @pytest.mark.fail_snapshot
-    @pytest.mark.calico_ci
+    # FIXME(apanchenko): uncomment as soon as the following bug is fixed
+    # FIXME(apanchenko): https://mirantis.jira.com/browse/PROD-12532
+    #@pytest.mark.calico_ci
     def test_calico_network_policies(self, show_step, config, underlay,
                                      k8s_deployed):
         """Test for deploying k8s environment with Calico and check
