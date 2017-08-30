e45f3350433b93c0ab74c3b5d96166c8f78c68c5
Tatyana Leontovich <tleontovich@mirantis.com>
2017-08-30 20:51:20 +0300

    tmp
    Change-Id: Id12c631aca99ecff82c4d36bfc4f278cd52bcbe4

--- tcp_tests/tests/system/test_install_mcp11_ovs_ocata.py
+++ tcp_tests/tests/system/test_install_mcp11_ovs_ocata.py
@@ -55,9 +55,62 @@ class Test_Mcp11_install(object):
         1. Prepare salt on hosts
         2. Setup controller nodes
         3. Setup compute nodes
+        4. Get monitoring nodes
+        5. Check that docker services are running
+        6. Check expected targets are ok
+        7. Check grafana dashboards
 
         """
         LOG.info("*************** DONE **************")
+        expected_service_list = ['monitoring_remote_storage_adapter',
+                                 'monitoring_server',
+                                 'monitoring_remote_agent',
+                                 'dashboard_grafana',
+                                 'monitoring_alertmanager',
+                                 'monitoring_remote_collector',
+                                 'monitoring_pushgateway']
+        # STEP #4
+        show_step(4)
+        mon_nodes = [node_name for
+                     node_name in underlay.node_names()
+                     if 'mon' in node_name]
+        show_step(5)
+        for node in mon_nodes:
+            service_stat_dict = {}
+            with underlay.remote(node_name=node) as node_remote:
+                result = node_remote.execute(
+                    "docker service ls --format '{{.Name}}:{{.Replicas}}'")
+                LOG.debug("Service ls result {0} "
+                          "from node {1}".format(result, node))
+                formatted_res = result['stdout'].splitlines()
+                for line in formatted_res:
+                    tmp = line.split(':')
+                    service_stat_dict.update({tmp[0]: tmp[1]})
+                assert len(service_stat_dict) == len(expected_service_list), \
+                    'Some services are mossied on node {0}. ' \
+                    'Current service list {1}'.format(node, service_stat_dict)
+                for service in expected_service_list:
+                    assert service in service_stat_dict,\
+                        'Missing service {0} in {1}'.format(
+                            service, service_stat_dict)
+                    assert '0' not in service_stat_dict.get(service),\
+                        'Service {0} failed to start'.format(service)
+        show_step(6)
+        prometheus_client = sl_deployed.api
+        try:
+            current_targets = prometheus_client.get_targets()
+            LOG.debug('Current targets after install {0}'.format(current_targets))
+        except:
+            LOG.warning('Restarting keepalived service on mon nodes...')
+            sl_actions._salt.local(tgt='mon*', fun='cmd.run',
+                                   args='systemctl restart keepalived')
+            LOG.warning(
+                'Ip states after force restart {0}'.format(
+                    sl_actions._salt.local(tgt='mon*',
+                                           fun='cmd.run', args='ip a')))
+            current_targets = prometheus_client.get_targets()
+            LOG.debug('Current targets after install {0}'.format(current_targets))
+
 
     @pytest.mark.fail_snapshot
     def test_mcp11_ocata_dpdk_install(self, underlay, openstack_deployed,
