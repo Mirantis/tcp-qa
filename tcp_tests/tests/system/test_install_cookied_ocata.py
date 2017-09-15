e1ba618912f0add2caeeb85c1170020c7d8097b5
Dennis Dmitriev <ddmitriev@mirantis.com>
2017-09-15 16:04:47 +0300

    Add 'cookied-mcp-ocata-dop-sl2' lab for DOP+SLv2
    * add workflow template and fuel-devops config
    * add new environment inventory file with mixed roles on nodes
    * add MAKE_SNAPSHOT_STAGES (default=true) to skip snapshots
      during deploy and not cause timesync and keepalived issues
    
    Change-Id: I29a504b783a017ef01d0b05535c3a03e7b1574c9

--- tcp_tests/tests/system/test_install_cookied_ocata.py
+++ tcp_tests/tests/system/test_install_cookied_ocata.py
@@ -46,3 +46,49 @@ class Test_Mcp11_install(object):
 
         """
         LOG.info("*************** DONE **************")
+
+    @pytest.mark.fail_snapshot
+    def test_cookied_ocata_cicd_oss_install(self, underlay, oss_deployed,
+                                            openstack_deployed, sl_deployed,
+                                            show_step):
+        """Test for deploying an mcp environment and check it
+        Scenario:
+        1. Prepare salt on hosts
+        2. Setup CICD nodes
+        3. Setup OpenStack
+        4. Setup StackLight v2
+        5. Get monitoring nodes
+        6. Check that docker services are running
+        7. Check current prometheus targets are UP
+        8. Run SL component tests
+        9. Download SL component tests report
+        """
+        expected_service_list = ['monitoring_remote_storage_adapter',
+                                 'monitoring_server',
+                                 'monitoring_remote_agent',
+                                 'dashboard_grafana',
+                                 'monitoring_alertmanager',
+                                 'monitoring_remote_collector',
+                                 'monitoring_pushgateway']
+        show_step(5)
+        mon_nodes = sl_deployed.get_monitoring_nodes()
+        LOG.debug('Mon nodes list {0}'.format(mon_nodes))
+
+        show_step(6)
+        sl_deployed.check_docker_services(mon_nodes, expected_service_list)
+
+        show_step(7)
+        sl_deployed.check_prometheus_targets(mon_nodes)
+
+        show_step(8)
+        # Run SL component tetsts
+        sl_deployed.run_sl_functional_tests(
+            'cfg01',
+            '/root/stacklight-pytest/stacklight_tests/tests/prometheus')
+
+        show_step(9)
+        # Download report
+        sl_deployed.download_sl_test_report(
+            'cfg01',
+            '/root/stacklight-pytest/stacklight_tests')
+        LOG.info("*************** DONE **************")
