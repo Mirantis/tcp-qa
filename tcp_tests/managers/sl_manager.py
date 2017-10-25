4fab65f2c18a4c2981f1f25824031b72018fa46b
sgudz <sgudz@mirantis.com>
2017-10-25 16:51:56 +0300

    Fix target and pillar for VIP determining.
    Change-Id: I774bbbe39374177527f9ad5cc73b4a5fe25fd201

--- tcp_tests/managers/sl_manager.py
+++ tcp_tests/managers/sl_manager.py
@@ -45,13 +45,13 @@ class SLManager(ExecuteCommandsMixin):
 
     def get_sl_vip(self):
         sl_vip_address_pillars = self._salt.get_pillar(
-            tgt='I@keepalived:cluster:enabled:true and not ctl*',
-            pillar='keepalived:cluster:instance:prometheus_server_vip:address')
+            tgt='I@prometheus:server:enabled:True',
+            pillar='keepalived:cluster:instance:VIP:address')
         sl_vip_ip = set([ip
                          for item in sl_vip_address_pillars
                          for node, ip in item.items() if ip])
         assert len(sl_vip_ip) == 1, (
-            "Found more than one SL VIP in pillars:{0}, "
+            "SL VIP not found or found more than one SL VIP in pillars:{0}, "
             "expected one!").format(sl_vip_ip)
         sl_vip_ip_host = sl_vip_ip.pop()
         return sl_vip_ip_host
