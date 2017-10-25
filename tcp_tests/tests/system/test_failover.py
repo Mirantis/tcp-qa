90401fb67e15ca3e48292721f93f6ad226ef1081
Tatyana Leontovich <tleontovich@mirantis.com>
2017-10-25 22:21:18 +0300

    Add test res check after reboot
    Change-Id: I21a123fa6ebb0f71250106f6df68cd5f412ad9fc

--- tcp_tests/tests/system/test_failover.py
+++ tcp_tests/tests/system/test_failover.py
@@ -11,7 +11,6 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-
 import pytest
 
 from tcp_tests import logger
@@ -148,8 +147,10 @@ class TestFailover(object):
             1. Prepare salt on hosts
             2. Setup controller nodes
             3. Setup compute, monitoring nodes
-            4. Restart mon01
-            5. Run LMA smoke after failover
+            4. Check targets before restart
+            5. Restart mon01
+            6. Check targets after restart
+            6. Run LMA smoke after failover
 
 
         """
@@ -160,15 +161,30 @@ class TestFailover(object):
 
         # STEP #4
         show_step(4)
-        openstack_actions.warm_restart_nodes('mon01')
+        mon_nodes = sl_deployed.get_monitoring_nodes()
+        LOG.debug('Mon nodes list {0}'.format(mon_nodes))
+        sl_deployed.check_prometheus_targets(mon_nodes)
+        before_result = sl_deployed.run_sl_tests_json(
+            'cfg01', '/root/stacklight-pytest/stacklight_tests/',
+            'tests/prometheus/', 'test_alerts.py')
+        failed_tests = [test['name'] for test in
+                        before_result if 'passed' not in test['outcome']]
         # STEP #5
         show_step(5)
+        openstack_actions.warm_restart_nodes('mon01')
+        # STEP #6
+        show_step(6)
+        sl_deployed.check_prometheus_targets(mon_nodes)
+        # STEP #7
+        show_step(7)
         # Run SL component tetsts
-        sl_deployed.run_sl_functional_tests(
-            'cfg01',
-            '/root/stacklight-pytest/stacklight_tests/',
-            'tests/prometheus/test_smoke.py',
-            'test_alerts.py')
+        after_result = sl_deployed.run_sl_tests_json(
+            'cfg01', '/root/stacklight-pytest/stacklight_tests/',
+            'tests/prometheus/', 'test_alerts.py')
+        for test in after_result:
+            if test['name'] not in failed_tests:
+                assert 'passed' in test['outcome'], \
+                    'Failed test {}'.format(test)
         LOG.info("*************** DONE **************")
 
     @pytest.mark.grab_versions
