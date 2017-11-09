af990ae8b2c54040d39692a645645ee69ac9dc53
sgudz <sgudz@mirantis.com>
2017-11-09 13:29:51 +0200

    Add test model with SLv1 for SLv2 upgrade Add Stacklight_V1 deploy steps Add workarounds for PROD-15965 and PROD-15966 Add steps for StackLight update Change-Id: I1fe4d31bd006b7a767d80ddeee83b4c2c7c90a4b

---
+++ tcp_tests/tests/system/test_upgrade_stacklight.py
@@ -0,0 +1,53 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import pytest
+
+from tcp_tests import logger
+
+LOG = logger.logger
+
+
+@pytest.mark.deploy
+class TestUpgradeStacklight(object):
+    """Test class for testing OpenContrail on a TCP lab"""
+
+    @pytest.mark.grab_versions
+    @pytest.mark.fail_snapshot
+    def test_upgrade_stacklight(self, underlay, config, sl_v1_upgraded):
+        """Runner for Juniper contrail-tests
+
+        Scenario:
+            1. Prepare salt on hosts
+            2. Setup controller nodes
+            3. Setup compute nodes
+            4. Prepare contrail-tests on ctl01 node
+            5. Run contrail-tests
+        """
+        mon_nodes = sl_v1_upgraded.get_monitoring_nodes()
+        LOG.debug('Mon nodes list {0}'.format(mon_nodes))
+
+
+        # Run SL component tetsts
+#        sl_deployed.run_sl_functional_tests(
+#            'cfg01',
+#            '/root/stacklight-pytest/stacklight_tests/',
+#            'tests/prometheus',
+#            'test_alerts.py')
+        # Download report
+#        sl_deployed.download_sl_test_report(
+#            'cfg01',
+#            '/root/stacklight-pytest/stacklight_tests/report.xml')
+#        LOG.info("*************** DONE **************")
+        LOG.info("*************** DONE **************")
