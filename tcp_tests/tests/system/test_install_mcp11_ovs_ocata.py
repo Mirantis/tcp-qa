a24312fe747eebb44b37459809c6d7853d83069c
Dmitry Tyzhnenko <t.dmitry@gmail.com>
2017-11-27 19:18:24 +0200

    HA Prometheus tests
    Change-Id: I3cb1c73a3088502a58c79f597bb640d7a8d505bd

--- tcp_tests/tests/system/test_install_mcp11_ovs_ocata.py
+++ tcp_tests/tests/system/test_install_mcp11_ovs_ocata.py
@@ -39,8 +39,8 @@ class Test_Mcp11_install(object):
 
         """
         openstack_actions._salt.local(
-                tgt='*', fun='cmd.run',
-                args='service ntp stop; ntpd -gq; service ntp start')
+            tgt='*', fun='cmd.run',
+            args='service ntp stop; ntpd -gq; service ntp start')
 
         if settings.RUN_TEMPEST:
             openstack_actions.run_tempest(pattern=settings.PATTERN)
