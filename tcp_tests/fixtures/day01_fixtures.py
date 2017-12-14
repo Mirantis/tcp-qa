79b12ee8025271e8b51bdd415b9be4eefc8b0744
Dmitry Tyzhnenko <t.dmitry@gmail.com>
2017-12-14 14:14:42 +0200

    Template and deploy test for day01 deployment
    Add physical-mcp-ocata-offline-ovs template
    Add day01 offline test
    
    Change-Id: I61211150575a84d8f3e44be2ef9c64855e5f2db3

---
+++ tcp_tests/fixtures/day01_fixtures.py
@@ -0,0 +1,55 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import pytest
+
+
+@pytest.fixture(scope='function')
+def day01_maas(request, config, hardware):
+    """Fixture that provides various actions for TCP
+
+    :param request: fixture provides pytest data
+    :param config: fixture provides oslo.config
+    :param underlay: fixture provides underlay manager
+    :rtype: RallyManager
+
+    For use in tests or fixtures to deploy a custom TCP
+    """
+    with_rally = request.keywords.get('with_rally', None)
+    rally_node = "gtw01."
+    if with_rally:
+        rally_node = with_rally.kwargs.get("rally_node", "gtw01.")
+
+    return rallymanager.RallyManager(underlay, rally_node)
+
+
+@pytest.fixture(scope='function')
+def day01_underlay(request, config, hardware):
+    """Fixture that provides various actions for TCP
+
+    :param request: fixture provides pytest data
+    :param config: fixture provides oslo.config
+    :param underlay: fixture provides underlay manager
+    :rtype: RallyManager
+
+    For use in tests or fixtures to deploy a custom TCP
+    """
+    with_rally = request.keywords.get('with_rally', None)
+    rally_node = "gtw01."
+    if with_rally:
+        rally_node = with_rally.kwargs.get("rally_node", "gtw01.")
+
+    return rallymanager.RallyManager(underlay, rally_node)
+
+
---
+++ tcp_tests/managers/jenkins/client.py
@@ -0,0 +1,65 @@
+import time
+
+import jenkins
+
+from devops.helpers import helpers
+
+
+class JenkinsClient(object):
+
+    def __init__(self, host=None, username=None, password=None):
+        host = host or 'http://172.16.44.33:8081'
+        username = username or 'admin'
+        password = password or 'r00tme'
+        self.__client = jenkins.Jenkins(
+            host,
+            username=username,
+            password=password)
+
+    def jobs(self):
+        return self.__client.get_jobs()
+
+    def find_jobs(self, name):
+        return filter(lambda x: name in x['fullname'], self.jobs())
+
+    def job_info(self, name):
+        return self.__client.get_job_info(name)
+
+    def list_builds(self, name):
+        return self.job_info(name).get('builds')
+
+    def build_info(self, name, build_id):
+        return self.get_build_info(name, build_id)
+
+    def job_params(self, name):
+        job = self.job_info(name)
+        job_params = next(
+            p for p in job['property'] if
+            'hudson.model.ParametersDefinitionProperty' == p['_class'])
+        job_params = job_params['parameterDefinitions']
+        return job_params
+
+    def make_defults_params(self, name):
+        job_params = self.job_params(name)
+        def_params = dict(
+            [(j['name'], j['defaultParameterValue']['value'])
+             for j in job_params])
+        return def_params
+
+    def run_build(self, name, params=None):
+        params = params or self.make_defults_params(name)
+        self.__client.build_job(name, params)
+        time.sleep(10)  # wait while jobs started:
+        build_id = self.job_info(name)['lastBuild']['number']
+        return name, build_id
+
+    def wait_end_of_build(self, name, build_id, timeout=600):
+
+        def building():
+            return not self.build_info(name, build_id)['building']
+
+        helpers.wait(
+            building,
+            timeout=timeout,
+            timeout_msg='Timeout waiting, job {0} are not finished "{1}" build'
+                        ' still'.format(name, build_id))
