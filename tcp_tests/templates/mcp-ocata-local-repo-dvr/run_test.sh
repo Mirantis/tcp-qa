d0afa157fadc2f9a98e13a3fd591ffbff9e36184
Dennis Dmitriev <ddmitriev@mirantis.com>
2017-12-22 16:35:57 +0200

    Fix offline deploy
    Change-Id: I0fd7418ad70985803a8c386c29dc379e8ad8ef1a

--- tcp_tests/templates/mcp-ocata-local-repo-dvr/run_test.sh
+++ tcp_tests/templates/mcp-ocata-local-repo-dvr/run_test.sh
@@ -14,7 +14,7 @@ export UBUNTU_SECURITY_REPOSITORY="deb http://mirror.mcp.mirantis.local.test/${R
 # Offline deployment simulation, requests to the apt01 node are redirected to an 'offline apt node' with mirrors of repositories
 export FORMULA_REPOSITORY="deb [arch=amd64] http://apt.mirantis.local.test/ubuntu-xenial ${REPOSITORY_SUITE} salt extra"
 export FORMULA_GPG="http://apt.mirantis.local.test/public.gpg"
-export SALT_REPOSITORY="deb [arch=amd64] http://apt.mirantis.local.test/ubuntu-xenial/salt/2016.3/ ${REPOSITORY_SUITE} main"
+export SALT_REPOSITORY="deb [arch=amd64] http://apt.mirantis.local.test/ubuntu-xenial/ ${REPOSITORY_SUITE} salt/2016.3 main"
 export SALT_GPG="http://apt.mirantis.local.test/public.gpg"
 export UBUNTU_REPOSITORY="deb http://mirror.mcp.mirantis.local.test/ubuntu xenial main universe restricted"
 export UBUNTU_UPDATES_REPOSITORY="deb http://mirror.mcp.mirantis.local.test/ubuntu xenial-updates main universe restricted"
