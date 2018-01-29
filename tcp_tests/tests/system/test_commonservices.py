607434f493994ac1944759e96e4ab3d40b6c2afb
sgudz <sgudz@mirantis.com>
2018-01-29 18:50:01 +0200

    Added model for initial ocata deploy with cicd
    Change-Id: I9854b32869251582e2bb52dcae9d09053d734a82

---
+++ tcp_tests/tests/system/test_commonservices.py
@@ -0,0 +1,46 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import pytest
+
+from tcp_tests import logger
+#from tcp_tests import settings
+
+LOG = logger.logger
+
+
+@pytest.mark.deploy
+class TestOpenContrail(object):
+    """Test class for testing OpenContrail on a TCP lab"""
+
+    @pytest.mark.fail_snapshot
+    def test_commonservices(self, config, common_services_deployed,
+                            show_step):
+        """Runner for Juniper contrail-tests
+
+        Scenario:
+            1. Prepare salt on hosts
+            2. Setup controller nodes
+            3. Setup compute nodes
+            4. Prepare contrail-tests on ctl01 node
+            5. Run contrail-tests
+        """
+        LOG.info("*************** DONE **************")
+
+        # opencontrail.prepare_tests(
+        #     config.opencontrail.opencontrail_prepare_tests_steps_path)
+
+        # opencontrail.run_tests(
+        #     tags=config.opencontrail.opencontrail_tags,
+        #     features=config.opencontrail.opencontrail_features)
