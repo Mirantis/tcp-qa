9056b98dc731bc7f768c7dd511b47a9b24111e46
sgudz <sgudz@mirantis.com>
2018-02-06 12:51:42 +0200

    Added model for initial ocata with contrail deploy
    Change-Id: I68285b08031ce24b38773a08981b439f4f50efa6

--- tcp_tests/tests/system/test_opencontrail.py
+++ tcp_tests/tests/system/test_opencontrail.py
@@ -25,6 +25,7 @@ class TestOpenContrail(object):
     """Test class for testing OpenContrail on a TCP lab"""
 
     @pytest.mark.fail_snapshot
+    @pytest.mark.with_rally(rally_node="ctl01.", prepare_openstack=True)
     def test_opencontrail(self, config, openstack_deployed,
                           show_step, sl_deployed):
         """Runner for Juniper contrail-tests
@@ -39,16 +40,3 @@ class TestOpenContrail(object):
         openstack_deployed._salt.local(
             tgt='*', fun='cmd.run',
             args='service ntp stop; ntpd -gq; service ntp start')
-
-        if settings.RUN_TEMPEST:
-            # openstack_deployed.run_tempest(target='ctl01',
-            #                               pattern=settings.PATTERN)
-            openstack_deployed.download_tempest_report(stored_node='cfg01')
-        LOG.info("*************** DONE **************")
-
-        # opencontrail.prepare_tests(
-        #     config.opencontrail.opencontrail_prepare_tests_steps_path)
-
-        # opencontrail.run_tests(
-        #     tags=config.opencontrail.opencontrail_tags,
-        #     features=config.opencontrail.opencontrail_features)
