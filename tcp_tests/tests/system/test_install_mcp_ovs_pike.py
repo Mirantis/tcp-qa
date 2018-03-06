2ac9e7c37a3811343aa38d1d9bc51182ecc802d3
Dennis Dmitriev <ddmitriev@mirantis.com>
2018-03-07 00:50:35 +0200

    Increase resources on gtw nodes for tempest
    Change-Id: I1d8d803c34de7343a430d40881772aaf69c8e224

--- tcp_tests/tests/system/test_install_mcp_ovs_pike.py
+++ tcp_tests/tests/system/test_install_mcp_ovs_pike.py
@@ -143,7 +143,7 @@ class TestMcpInstallOvsPike(object):
 
     @pytest.mark.grab_versions
     @pytest.mark.fail_snapshot
-    def test_mcp11_pike_dpdk_install(self, underlay, openstack_deployed,
+    def test_mcp_pike_dpdk_install(self, underlay, openstack_deployed,
                                      show_step, openstack_actions):
         """Test for deploying an mcp dpdk environment and check it
         Scenario:
