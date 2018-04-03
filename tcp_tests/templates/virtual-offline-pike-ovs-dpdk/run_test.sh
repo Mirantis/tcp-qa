d34f4103464b6232cf0f4ce13a629e4a726561e5
Tatyana Leontovich <tleontovich@mirantis.com>
2018-04-03 17:49:24 +0300

    Initial integration L2EGW and DGP to offline
    Change-Id: I392b146c1c880e1fe87335172d6e45dc68212f05

---
+++ tcp_tests/templates/virtual-offline-pike-ovs-dpdk/run_test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+. /home/jenkins/fuel-devops30/bin/activate
+pip install -r ./tcp_tests/requirements.txt -U
+pip install psycopg2
+
+export ENV_NAME=virtual-offline-e-dpdk
+export VENV_PATH=/home/jenkins/fuel-devops30
+export IMAGE_PATH1604=/home/jenkins/images/xenial-server-cloudimg-amd64.qcow2
+export SHUTDOWN_ENV_ON_TEARDOWN=false
+export PYTHONIOENCODING=UTF-8
+export LAB_CONFIG_NAME=virtual-offline-pike-ovs-dpdk
+export CLUSTER_NAME=virtual-offline-pike-ovs-dpdk
+export REPOSITORY_SUITE=2018.3.0
+
+export TEST_GROUP=test_mcp_pike_ovs_install
+export RUN_TEMPEST=true
+
+# Offline deploy parameters
+export SALT_MODELS_REF_CHANGE=refs/changes/44/15144/1
+
+export BOOTSTRAP_TIMEOUT=1200
+
+export HOST_APT=10.170.0.226
+export HOST_SALTSTACK=10.170.0.226
+export HOST_ARCHIVE_UBUNTU=10.170.0.226
+export HOST_MIRROR_MCP_MIRANTIS=10.170.0.226
+export HOST_MIRROR_FUEL_INFRA=10.170.0.226
+export HOST_PPA_LAUNCHPAD=10.170.0.226
+
+export SALT_MODELS_SYSTEM_REPOSITORY=https://gerrit.mcp.mirantis.local.test/salt-models/reclass-system
+export SALT_FORMULAS_REPO=https://gerrit.mcp.mirantis.local.test/salt-formulas
+export FORMULA_REPOSITORY="deb [arch=amd64] http://apt.mirantis.local.test/ubuntu-xenial ${REPOSITORY_SUITE} salt extra"
+export FORMULA_GPG="http://apt.mirantis.local.test/public.gpg"
+export SALT_REPOSITORY="deb [arch=amd64] http://apt.mirantis.local.test/ubuntu-xenial/ ${REPOSITORY_SUITE} salt/2016.3 main"
+export SALT_GPG="http://apt.mirantis.local.test/public.gpg"
+export UBUNTU_REPOSITORY="deb http://mirror.mcp.mirantis.local.test/ubuntu xenial main universe restricted"
+export UBUNTU_UPDATES_REPOSITORY="deb http://mirror.mcp.mirantis.local.test/ubuntu xenial-updates main universe restricted"
+export UBUNTU_SECURITY_REPOSITORY="deb http://mirror.mcp.mirantis.local.test/ubuntu xenial-security main universe restricted"
+
+cd tcp_tests
+py.test -vvv -s -p no:django -p no:ipdb --junit-xml=nosetests.xml -k ${TEST_GROUP}
