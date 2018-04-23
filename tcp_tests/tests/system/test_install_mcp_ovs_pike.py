91e7f52eae7c7cf753469ed47e4b62bc24b02aa7
Oleksii Butenko <obutenko@mirantis.com>
2018-04-23 16:19:11 +0300

    Add test for bm deploy without grab_versions
    Change-Id: Ie8ff513161e57048dc92aadad52357a5fa9e91dc

--- tcp_tests/tests/system/test_install_mcp_ovs_pike.py
+++ tcp_tests/tests/system/test_install_mcp_ovs_pike.py
@@ -260,3 +260,19 @@ class TestMcpInstallOvsPike(object):
             )
             openstack_actions.download_tempest_report()
         LOG.info("*************** DONE **************")
+
+    @pytest.mark.fail_snapshot
+    def test_mcp_pike_ovs_bm_install(self, underlay, openstack_deployed,
+                                   config, show_step, openstack_actions):
+        """Test for deploying an mcp dpdk environment and check it
+        Scenario:
+        1. Prepare salt on hosts
+        2. Setup controller nodes
+        3. Setup compute nodes
+        """
+        LOG.info("*************** DONE **************")
+        openstack_actions._salt.local(
+            tgt='*', fun='cmd.run',
+            args='service ntp stop; ntpd -gq; service ntp start')
+
+        LOG.info("*************** DONE **************")
