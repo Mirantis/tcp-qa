4adcfe8d5af5842bdad2f69c8329c933e317f9bc
Vladimir Jigulin <vjigulin@mirantis.com>
2018-05-28 18:17:01 +0400

    [WIP] Add k8s chain-update test
    Change-Id: Ica0cdf83e3a3b3a74e1eef8557d7064e97db1967

--- tcp_tests/tests/system/test_k8s_actions.py
+++ tcp_tests/tests/system/test_k8s_actions.py
@@ -77,3 +77,31 @@ class TestMCPK8sActions(object):
 
         show_step(1)
         k8s_deployed.start_k8s_cncf_verification()
+
+    @pytest.mark.grap_versions
+    @pytest.mark.fail_snapshot
+    def test_k8s_chain_update(self, show_step, config, k8s_deployed,
+                              k8s_chain_update_log_helper):
+        """Test for chain-upgrading k8s hypercube pool and checking it
+
+        Scenario:
+            1. Prepare salt on hosts
+            2. Setup controller nodes
+            3. Setup compute nodes
+            4. Setup Kubernetes cluster
+            5. Update hypercube, update pool
+            6. Run conformance for updated cluster
+            7. Go to step 5 if update chain is not ended
+        """
+
+        chain_versions = config.k8s.k8s_update_chain
+
+        for version in chain_versions:
+            show_step(5)
+            k8s_deployed.update_k8s_images(version)
+
+            show_step(6)
+            LOG.info("Running conformance on {} version".format(version))
+            k8s_deployed.run_conformance(log_out="k8s_conformance_{}.log".format(chain_versions.index(version)))
+
+            show_step(7)
