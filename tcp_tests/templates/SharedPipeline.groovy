cb2321f5948abd73a2820534cb2d6a838dcf6918
Dennis Dmitriev <ddmitriev@mirantis.com>
2018-06-11 12:57:13 +0300

    wip
    Change-Id: I958d183d8951b869877f0c36f4bd1000a5b7e6a9

---
+++ tcp_tests/templates/SharedPipeline.groovy
@@ -0,0 +1,36 @@
+common = new com.mirantis.mk.Common()
+
+def run_cmd(cmd, returnStdout=false) {
+    common.printMsg("Run shell command:\n" + cmd, "blue")
+    def VENV_PATH='/home/jenkins/fuel-devops30'
+    script = """\
+        set +x;
+        echo 'activate python virtualenv ${VENV_PATH}';
+        . ${VENV_PATH}/bin/activate;
+        bash -c 'set -ex; set -ex; ${cmd.stripIndent()}'
+    """
+    return sh(script: script, returnStdout: returnStdout)
+}
+
+def run_cmd_stdout(cmd) {
+    return run_cmd(cmd, true)
+}
+
+def clean_working_dir() {
+    println "Clean the working directory ${env.WORKSPACE}"
+    deleteDir()
+    // do not fail if environment doesn't exists
+    println "Remove environment ${ENV_NAME}"
+    run_cmd("""\
+        dos.py erase ${ENV_NAME} || true
+    """)
+    println "Remove config drive ISO"
+    run_cmd("""\
+        rm /home/jenkins/images/${CFG01_CONFIG_IMAGE_NAME} || true
+    """)
+}
+
+
+
+// pretend a groovy class, DO NOT REMOVE
+return this
