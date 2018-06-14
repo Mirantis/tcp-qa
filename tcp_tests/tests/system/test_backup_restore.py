02b8218086f7501fe00e1a96d592175450de4b4d
Tatyana Leontovich <tleontovich@mirantis.com>
2018-06-14 16:42:23 +0300

    WIP restore cfg
    Change-Id: Icbc9a86c1fb3e5f89b0852eaf9720c446d66519a

---
+++ tcp_tests/tests/system/test_backup_restore.py
@@ -0,0 +1,63 @@
+#    Copyright 2018 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from tcp_tests import logger
+from tcp_tests.managers import backup_restore_manager
+
+LOG = logger.logger
+
+
+class TestBackupRestoreMaster(object):
+    """Test class for testing backup restore of master node"""
+
+    def test_backup_cfg_backupninja_rsync(
+            self, underlay, config, openstack_deployed, salt_actions, show_step):
+        """Test add policy for Nova service
+
+        Scenario:
+            1. Prepare salt on hosts
+            2. Setup controller nodes
+            3. Setup compute nodes
+            4. Check config for rsync exists
+            5. Run backup command
+            6. Delete reclass files on master node
+            7. Run restore
+            8. Check reclass was restored
+            9. Check minions work fine with master
+
+        """
+        backup = backup_restore_manager.BackupRestoreManager(
+            config=config, underlay=underlay, salt_api=salt_actions)
+        # STEP #1,2,3
+        show_step(1)
+        show_step(2)
+        show_step(3)
+
+        # STEP #4
+        show_step(4)
+        backup.check_file_exists('cfg01')
+
+        # STEP #5
+        show_step(5)
+        backup.create_backup('cfg01')
+
+        # STEP #6
+        show_step(6)
+        #backup.delete_dirs_files('cfg01')
+
+        # STEP #7
+        show_step(7)
+        backup.restore_salt('cfg01')
+
+        LOG.info("*************** DONE **************")
