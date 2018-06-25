8f62740b0d209db0e62d9ec5e2defa5bac4158fe
Vladimir Jigulin <vjigulin@mirantis.com>
2018-06-25 13:00:51 +0400

    [wip] k8s keepalived failover test
    Change-Id: I668844f91f4a8fb473e199977f1ebe6ca7ddc35a

--- tcp_tests/fixtures/k8s_fixtures.py
+++ tcp_tests/fixtures/k8s_fixtures.py
@@ -23,7 +23,7 @@ LOG = logger.logger
 
 
 @pytest.fixture(scope='function')
-def k8s_actions(config, underlay, salt_deployed):
+def k8s_actions(config, underlay, salt_deployed, hardware):
     """Fixture that provides various actions for K8S
 
     :param config: fixture provides oslo.config
@@ -33,7 +33,7 @@ def k8s_actions(config, underlay, salt_deployed):
 
     For use in tests or fixtures to deploy a custom K8S
     """
-    return k8smanager.K8SManager(config, underlay, salt_deployed)
+    return k8smanager.K8SManager(config, underlay, salt_deployed, hardware)
 
 
 @pytest.mark.revert_snapshot(ext.SNAPSHOT.k8s_deployed)
