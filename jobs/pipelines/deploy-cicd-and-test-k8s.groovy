9931e682c5b87b965ad894322b9654d801494a14
Dennis Dmitriev <ddmitriev@mirantis.com>
2018-07-08 21:23:00 +0300

    Add cookied-cicd models for openstack configs: ovs/dvr/dpdk
    Change-Id: If9e72928b90bb13fd51199c93f9255ec52576bc4

--- jobs/pipelines/deploy-cicd-and-test-k8s.groovy
+++ jobs/pipelines/deploy-cicd-and-test-k8s.groovy
@@ -3,6 +3,7 @@
 common = new com.mirantis.mk.Common()
 shared = new com.mirantis.system_qa.SharedPipeline()
 
+
 node ("${NODE_NAME}") {
   try {
 
@@ -10,24 +11,16 @@ node ("${NODE_NAME}") {
         shared.prepare_working_dir()
     }
 
-    stage("Create environment, generate mode, bootstrap the salt-cluster") {
+    stage("Create environment, generate model, bootstrap the salt-cluster") {
         shared.swarm_bootstrap_salt_cluster_devops()
     }
 
-    // Install core and cicd
-    stage("Run Jenkins job on salt-master [deploy_openstack:core]") {
-        shared.run_job_on_day01_node("core")
+    stage("Install core infrastructure and deploy CICD nodes") {
+        shared.swarm_deploy_cicd('core,cicd')
     }
 
-    stage("Run Jenkins job on salt-master [deploy_openstack:cicd]") {
-        shared.run_job_on_day01_node("cicd")
-    }
-
-    // Install the cluster
-    for (stack in "${STACK_INSTALL}".split(",")) {
-        stage("Run Jenkins job on CICD [deploy_openstack:${stack}]") {
-            shared.run_job_on_cicd_nodes(stack)
-        }
+    stage("Install core infrastructure and deploy CICD nodes") {
+        shared.swarm_deploy_stacks(env.STACK_INSTALL)
     }
 
     stage("Run tests") {
@@ -36,11 +29,22 @@ node ("${NODE_NAME}") {
             . ./tcp_tests/utils/env_salt
             . ./tcp_tests/utils/env_k8s
 
+            # Prepare snapshots that may be used in tests if MANAGER=devops
+            cp \$(pwd)/${ENV_NAME}_salt_deployed.ini \$(pwd)/${ENV_NAME}_k8s_deployed.ini
+            cp \$(pwd)/${ENV_NAME}_salt_deployed.ini \$(pwd)/${ENV_NAME}_sl_deployed.ini
+            dos.py suspend ${ENV_NAME}
+            dos.py snapshot ${ENV_NAME} k8s_deployed
+            dos.py snapshot ${ENV_NAME} sl_deployed
+            dos.py resume ${ENV_NAME}
+            dos.py time-sync ${ENV_NAME}
+
             # Initialize variables used in tcp-qa tests
             export CURRENT_SNAPSHOT=sl_deployed  # provide the snapshot name required by the test
             export TESTS_CONFIGS=\$(pwd)/${ENV_NAME}_salt_deployed.ini  # some SSH data may be filled separatelly
 
-            export MANAGER=empty  # skip 'hardware' fixture, disable snapshot/revert features
+            #export MANAGER=empty  # skip 'hardware' fixture, disable snapshot/revert features
+            export MANAGER=devops  # use 'hardware' fixture to manage fuel-devops environment
+            export MAKE_SNAPSHOT_STAGES=false  # skip 'hardware' fixture, disable snapshot/revert features
             # export SSH='{...}'  # non-empty SSH required to skip 'underlay' fixture. It is filled from TESTS_CONFIGS now
             export salt_master_host=\$SALT_MASTER_IP  # skip salt_deployed fixture
             export salt_master_port=6969
@@ -52,21 +56,30 @@ node ("${NODE_NAME}") {
 
             py.test -vvv -s -p no:django -p no:ipdb --junit-xml=nosetests.xml -m k8s_calico_sl
 
-            #dos.py suspend ${ENV_NAME}
-            #dos.py snapshot ${ENV_NAME} test_completed
-            #dos.py resume ${ENV_NAME}
-            #dos.py time-sync ${ENV_NAME}
+            dos.py suspend ${ENV_NAME}
+            dos.py snapshot ${ENV_NAME} test_completed
             """)
     }
 
   } catch (e) {
+    shared.run_cmd("""\
+        dos.py suspend ${ENV_NAME}
+        dos.py snapshot ${ENV_NAME} test_failed
+        """)
       common.printMsg("Job failed", "red")
       throw e
   } finally {
     // TODO(ddmitriev): analyze the "def currentResult = currentBuild.result ?: 'SUCCESS'"
     // and report appropriate data to TestRail
-    shared.run_cmd("""\
-        dos.py destroy ${ENV_NAME}
-    """)
+    if ("${env.SHUTDOWN_ENV_ON_TEARDOWN}" == "true") {
+        shared.run_cmd("""\
+            dos.py destroy ${ENV_NAME}
+        """)
+    } else {
+        shared.run_cmd("""\
+            dos.py resume ${ENV_NAME}
+            dos.py time-sync ${ENV_NAME}
+        """)
+    }
   }
 }
