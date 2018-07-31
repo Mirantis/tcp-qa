3e35c3eb34ff3c577192f35b74319997079b99bc
Dennis Dmitriev <ddmitriev@mirantis.com>
2018-07-31 08:40:20 +0300

    Fix console output in run_cmd()
    Change-Id: Ib7f80c8f03aaeb7ce5117f3d2f57c6be70d1c1a9

--- src/com/mirantis/system_qa/SharedPipeline.groovy
+++ src/com/mirantis/system_qa/SharedPipeline.groovy
@@ -12,7 +12,7 @@ def run_cmd(String cmd, Boolean returnStdout=false, Boolean exeption_with_logs=f
         set +x;
         echo 'activate python virtualenv ${VENV_PATH}';
         . ${VENV_PATH}/bin/activate;
-        bash -c 'set -ex; set -ex; ${cmd.stripIndent()}' 1>${stdout_path} 2>${stderr_path}
+        bash -c 'set -ex; set -ex; ${cmd.stripIndent()}' 2>${stderr_path} | tee ${stdout_path}
     """
     def result
     try {
