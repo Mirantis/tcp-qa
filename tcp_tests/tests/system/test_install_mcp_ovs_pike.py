022dda266c10e2afafeefc4bfbc69a5d30b372a8
Tatyana Leontovich <tleontovich@mirantis.com>
2018-11-30 12:46:40 +0200

    [WIP]Split prepare_run_tempet
    To be possible to run tests without creation of yml,
    move this func to separate func
    
    Change-Id: I2393b7941725308c12b47708ef5b1e241807e33a

--- tcp_tests/tests/system/test_install_mcp_ovs_pike.py
+++ tcp_tests/tests/system/test_install_mcp_ovs_pike.py
@@ -50,6 +50,28 @@ class TestMcpInstallOvsPike(object):
 
     @pytest.mark.grab_versions
     @pytest.mark.fail_snapshot
+    @pytest.mark.pike_ovs_offline
+    def test_mcp_pike_ovs_offline_install(self, underlay,
+                                          openstack_actions,
+                                          tempest_actions):
+        """Test for deploying an mcp environment and check it
+        Scenario:
+        1. Prepare salt on hosts
+        2. Setup controller nodes
+        3. Setup compute nodes
+        4. Run tempest
+
+        """
+        openstack_actions._salt.local(
+            tgt='*', fun='cmd.run',
+            args='service ntp stop; ntpd -gq; service ntp start')
+        if settings.RUN_TEMPEST:
+            tempest_actions.start_tempest()
+
+        LOG.info("*************** DONE **************")
+
+    @pytest.mark.grab_versions
+    @pytest.mark.fail_snapshot
     @pytest.mark.pike_ovs_sl
     def test_mcp_pike_ovs_sl_install(self, underlay, config,
                                      openstack_deployed,
