283a036481bc6b41ecf3def8f1c06b4bbb50dd9f
Dmitry Tyzhnenko <t.dmitry@gmail.com>
2018-12-18 17:43:07 +0200

    Add dsl
    Change-Id: I7d7d2509d8df5d8803d72ebf6dbe458b4eb1aa2a

---
+++ jobs/pipelines/job-dsl-test.groovy
@@ -0,0 +1,18 @@
+node('cz8133') {
+    step([$class: 'WsCleanup'])
+    stage("Checkout review") {
+        checkout(
+            [$class: 'GitSCM',
+             branches: [[name: "${GERRIT_REFSPEC}"]],
+             doGenerateSubmoduleConfigurations: false,
+             extensions: [[$class: 'CleanBeforeCheckout']],
+             submoduleCfg: [],
+             userRemoteConfigs:
+             [[refspec: "+${GERRIT_REFSPEC}:refs/remotes/origin/${GERRIT_REFSPEC} +refs/heads/*:refs/remotes/origin/*",
+               url: 'https://review.gerrithub.io/Mirantis/tcp-qa']]])
+
+    }
+    stage("run Gradle tests") {
+        sh "./gradlew test"
+    }
+}
