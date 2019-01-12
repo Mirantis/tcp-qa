f537a53a8c84e288a0926cb209313b1df86bb376
obutenko <obutenko@mirantis.com>
2019-01-12 21:11:55 +0200

    Fix runtestmanager
    WIP
    
    Change-Id: I1051afe091d68264492a622b89d95828c47dfb61

--- tcp_tests/managers/runtestmanager.py
+++ tcp_tests/managers/runtestmanager.py
@@ -14,6 +14,7 @@
 
 import json
 import os
+import time
 
 from devops.helpers import helpers
 
@@ -104,6 +105,11 @@ class RuntestManager(object):
 
     def prepare(self):
         salt_call_cmd = "salt-call -l info --hard-crash --state-output=mixed "
+        barbican_pillar = "nova:controller:barbican:enabled"
+        control_name = 'ctl01.' + self.domain_name
+        result = self.__salt_api.get_pillar(tgt=control_name,
+                                            pillar=barbican_pillar)
+        barbican = result[0].get(self.control_name, False)
         commands = [
             {
                 'description': ("Install docker-ce package and "
@@ -124,7 +130,31 @@ class RuntestManager(object):
                 'cmd': ("set -ex;" +
                         "salt-run state.orchestrate " +
                         "runtest.orchestrate.tempest")},
+            {
+                'description': "Configure barbican",
+                'node_name': self.master_name,
+                'cmd': ("set -ex;" +
+                        salt_call_cmd +
+                        " state.sls barbican.client && " +
+                        salt_call_cmd +
+                        " state.sls runtest.test_accounts && " +
+                        salt_call_cmd +
+                        " state.sls runtest.barbican_sign_image")},
         ]
+
+        if barbican:
+            commands.append({
+                'description': "Configure barbican",
+                'node_name': self.master_name,
+                'cmd': ("set -ex;" +
+                        salt_call_cmd +
+                        " state.sls barbican.client && " +
+                        salt_call_cmd +
+                        " state.sls runtest.test_accounts && " +
+                        salt_call_cmd +
+                        " state.sls runtest.barbican_sign_image")},
+            )
+
         self.__salt_api.execute_commands(commands=commands,
                                          label="Prepare for Tempest")
 
