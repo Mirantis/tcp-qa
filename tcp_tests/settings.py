7967837091f43aaaabb426adb24d6ec7c4829e6a
vitalygusev <vgusev@mirantis.com>
2019-01-15 18:19:51 +0400

    Use gerrit project for stacklight-pytest
    Change-Id: I9596c2ddb4f2e90693b5f7b512f154c8d6278903

--- tcp_tests/settings.py
+++ tcp_tests/settings.py
@@ -85,8 +85,8 @@ TEMPEST_THREADS = int(os.environ.get('TEMPEST_THREADS', 2))
 TEMPEST_TARGET = os.environ.get('TEMPEST_TARGET', 'gtw01')
 SALT_VERSION = os.environ.get('SALT_VERSION', '2017.7')
 
-SL_TEST_REPO = os.environ.get('SL_TEST_REPO',
-                              'https://github.com/Mirantis/stacklight-pytest')
+SL_TEST_REPO = os.environ.get(
+    'SL_TEST_REPO', 'https://gerrit.mcp.mirantis.com/mcp/stacklight-pytest')
 SL_TEST_COMMIT = os.environ.get('SL_TEST_COMMIT', 'master')
 
 EXTERNAL_ADDRESS_POOL_NAME = os.environ.get('EXTERNAL_ADDRESS_POOL_NAME',
