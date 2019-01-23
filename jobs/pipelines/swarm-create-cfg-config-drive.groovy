9c9d5f4575ed89b9ef3b3a2755bf62e8945aabc6
PGlazov <pglazov@mirantis.com>
2019-01-23 18:42:50 +0400

    temporary add cookied-cicd-mitaka-dvr-sl
    Change-Id: I18502b6acd6f7fc0b0542415396fc9d5ff8b0b3d

--- jobs/pipelines/swarm-create-cfg-config-drive.groovy
+++ jobs/pipelines/swarm-create-cfg-config-drive.groovy
@@ -19,6 +19,7 @@ smc['MCP_VERSION'] = "${MCP_VERSION}"
 // smc['LOCAL_REPOS'] = 'true'
 smc['MCP_SALT_REPO_KEY'] = "${MCP_SALT_REPO_KEY}"
 smc['MCP_SALT_REPO_URL'] = "${MCP_SALT_REPO_URL}"
+smc['MCP_SALT_REPO_URL'] = "deb [arch=amd64] http://mirror.mirantis.com//2018.8.0/salt-formulas/xenial xenial main"
 
 def entries(m) {
     m.collect {k, v -> [k, v]}
