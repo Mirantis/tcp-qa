4594c4de3fe35bd0627e8995b369b94b1bbd7e85
sgudz <sgudz@mirantis.com>
2019-02-26 15:49:22 +0200

    test
    Change-Id: Ia6caf8639c0be03b63771a784e3cabf859b7bcf0

--- tcp_tests/managers/runtestmanager.py
+++ tcp_tests/managers/runtestmanager.py
@@ -192,7 +192,10 @@ class RuntestManager(object):
             inspect_res = self.salt_api.local(tgt,
                                               'dockerng.inspect',
                                               self.container_name)
-            if 'return' in inspect_res:
+            LOG.info("Target {}".format(tgt))
+            LOG.info("Container_name {}".format(self.container_name))
+            LOG.info("inspect_res {}".format(inspect_res))
+            if inspect_res.get('return', [{}]) != [{}]:
                 inspect = inspect_res['return']
                 inspect = inspect[0]
                 inspect = next(inspect.iteritems())[1]
