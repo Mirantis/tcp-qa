fde4d439fd02feda726407117c7af6685b39cf77
Dennis Dmitriev <ddmitriev@mirantis.com>
2019-03-22 11:22:32 +0200

    Add possibility to use ENV_MANAGER=heat in test pipelines
    - If ENV_MANAGER=heat, environment snapshots are unavailable.
      Carefully select the test cases for such environments.
    - Added a new job swarm-bootstrap-salt-cluster-heat.groovy
      to create environment in OpenStack
    - Added new parameters for parent jobs:
      ENV_MANAGER (default=devops)
      OS_AUTH_URL (for ENV_MANAGER=heat) - Keystone URL
      OS_PROJECT_NAME (for ENV_MANAGER=heat) - OS project name
      OS_CREDENTIALS (for ENV_MANAGER=heat) - Jenkins credentials
        with username and password to access OpenStack
      LAB_PARAM_DEFAULTS (for ENV_MANAGER=heat) - environment file
        for Heat template with 'parameter_defaults' dict.
    
    Change-Id: I5d36f383354932b55794ddfc99a7a1622c68f4ea

--- tcp_tests/settings_oslo.py
+++ tcp_tests/settings_oslo.py
@@ -22,15 +22,14 @@ from tcp_tests.helpers import ext
 from tcp_tests.helpers import oslo_cfg_types as ct
 from tcp_tests import settings
 
-print("\n" + "-" * 10 + " Initialize oslo.config variables with defaults"
-      " from environment" + "-" * 10)
 
 _default_conf = pkg_resources.resource_filename(
     __name__, 'templates/{0}/underlay.yaml'.format(settings.LAB_CONFIG_NAME))
 _default_heat_conf = pkg_resources.resource_filename(
     __name__, 'templates/{0}/underlay.hot'.format(settings.LAB_CONFIG_NAME))
 _default_heat_env = pkg_resources.resource_filename(
-    __name__, 'templates/_heat_environments/microcloud-8116.env')
+    __name__, 'templates/_heat_environments/{0}'.format(
+        settings.LAB_PARAM_DEFAULTS))
 
 _default_salt_steps = pkg_resources.resource_filename(
     __name__, 'templates/{0}/salt.yaml'.format(settings.LAB_CONFIG_NAME))
