7c106d281e2338a88cd770c70bd01f21f2f4bdb0
sgudz <sgudz@mirantis.com>
2019-03-22 16:14:00 +0200

    test model generation for heat
    Change-Id: I932800964dd3868eac0d023538b8cd28bb2ffa50

--- jobs/pipelines/swarm-create-cfg-config-drive.groovy
+++ jobs/pipelines/swarm-create-cfg-config-drive.groovy
@@ -133,6 +133,8 @@ node (node_name) {
             // Replace only if the variable is non-empty, leave default otherwise
             if (i[1]) {
                 sh "sed -i \"s,export ${i[0]}=.*,export ${i[0]}=${i[1]},\" ${user_data_script_path}"
+                sh "sed -i 's/\(^.*curl -s .*MCP_SALT_REPO_KEY.\).*/\1 > out.txt\n       apt-key add out.txt/g' ${user_data_script_path}"
+
             }
         }
     }
