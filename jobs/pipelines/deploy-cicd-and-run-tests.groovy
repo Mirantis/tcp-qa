9f3726094a116834ebce22be2d2e2d606fcb3c5e
Dennis Dmitriev <ddmitriev@mirantis.com>
2019-04-17 18:02:46 +0300

    Add MAKE_SNAPSHOT_STAGES job parameters
    - add MAKE_SNAPSHOT_STAGES to the parent and children jobs,
      True by default
    - MAKE_SNAPSHOT_STAGES is always disabled for ENV_MANAGER='heat'
    
    Change-Id: I2f1a8ae6cdc2f1be7a0d133d764ed3446305507a

--- jobs/pipelines/deploy-cicd-and-run-tests.groovy
+++ jobs/pipelines/deploy-cicd-and-run-tests.groovy
@@ -4,11 +4,19 @@ def common = new com.mirantis.mk.Common()
 def shared = new com.mirantis.system_qa.SharedPipeline()
 def steps = "hardware,create_model,salt," + env.DRIVETRAIN_STACK_INSTALL + "," + env.PLATFORM_STACK_INSTALL
 def env_manager = env.ENV_MANAGER ?: 'devops'
-def jenkins_slave_node_name = "${NODE_NAME}"
+
+if (env_manager == 'devops') {
+    jenkins_slave_node_name = "${NODE_NAME}"
+    make_snapshot_stages = env.MAKE_SNAPSHOT_STAGES ?: true
+} else if (env_manager == 'heat') {
+    jenkins_slave_node_name = "openstack_slave_${JOB_NAME}"
+    make_snapshot_stages = false
+}
 
 currentBuild.description = "${NODE_NAME}:${ENV_NAME}"
 
-def deploy(shared, common, steps, env_manager, jenkins_slave_node_name) {
+//def deploy(shared, common, steps, env_manager, jenkins_slave_node_name) {
+def deploy(shared, common, steps, env_manager) {
     def report_text = ''
     try {
 
@@ -19,15 +27,18 @@ def deploy(shared, common, steps, env_manager, jenkins_slave_node_name) {
         stage("Create environment, generate model, bootstrap the salt-cluster") {
             // steps: "hardware,create_model,salt"
             if (env_manager == 'devops') {
-                jenkins_slave_node_name = "${NODE_NAME}"
+//                jenkins_slave_node_name = "${NODE_NAME}"
                 shared.swarm_bootstrap_salt_cluster_devops()
             } else if (env_manager == 'heat') {
-                def new_jenkins_slave_node_name = "openstack_slave_${JOB_NAME}"
+//                def new_jenkins_slave_node_name = "openstack_slave_${JOB_NAME}"
                 // If shared.swarm_bootstrap_salt_cluster_heat() failed,
                 // do not schedule shared.swarm_testrail_report() on the non existing Jenkins slave
-                shared.swarm_bootstrap_salt_cluster_heat(new_jenkins_slave_node_name)
+//                shared.swarm_bootstrap_salt_cluster_heat(new_jenkins_slave_node_name)
+                shared.swarm_bootstrap_salt_cluster_heat(jenkins_slave_node_name)
                 // When the Heat stack created, set jenkins_slave_node_name to the new Jenkins slave
-                jenkins_slave_node_name = new_jenkins_slave_node_name
+//                jenkins_slave_node_name = new_jenkins_slave_node_name
+                // disable dos.py snapshots for 'heat' manager
+//                make_snapshot_stages = false
             } else {
                 throw new Exception("Unknow env_manager: '${env_manager}'")
             }
@@ -36,7 +47,7 @@ def deploy(shared, common, steps, env_manager, jenkins_slave_node_name) {
         stage("Install core infrastructure and deploy CICD nodes") {
         if (env.DRIVETRAIN_STACK_INSTALL) {
                 // steps: env.DRIVETRAIN_STACK_INSTALL
-                shared.swarm_deploy_cicd(env.DRIVETRAIN_STACK_INSTALL, env.DRIVETRAIN_STACK_INSTALL_TIMEOUT, jenkins_slave_node_name)
+                shared.swarm_deploy_cicd(env.DRIVETRAIN_STACK_INSTALL, env.DRIVETRAIN_STACK_INSTALL_TIMEOUT, jenkins_slave_node_name, make_snapshot_stages)
             } else {
                 common.printMsg("DRIVETRAIN_STACK_INSTALL is empty, skipping 'swarm-deploy-cicd' job", "green")
             }
@@ -45,7 +56,7 @@ def deploy(shared, common, steps, env_manager, jenkins_slave_node_name) {
         stage("Deploy platform components") {
             if (env.PLATFORM_STACK_INSTALL) {
                 // steps: env.PLATFORM_STACK_INSTALL
-                shared.swarm_deploy_platform(env.PLATFORM_STACK_INSTALL, env.PLATFORM_STACK_INSTALL_TIMEOUT, jenkins_slave_node_name)
+                shared.swarm_deploy_platform(env.PLATFORM_STACK_INSTALL, env.PLATFORM_STACK_INSTALL_TIMEOUT, jenkins_slave_node_name, make_snapshot_stages)
             } else {
                 common.printMsg("PLATFORM_STACK_INSTALL is empty, skipping 'swarm-deploy-platform' job", "green")
             }
@@ -56,7 +67,7 @@ def deploy(shared, common, steps, env_manager, jenkins_slave_node_name) {
     } catch (e) {
         common.printMsg("Deploy is failed: " + e.message , "purple")
         report_text = e.message
-        if (env_manager == 'devops') {
+        if (make_snapshot_stages) {
             def snapshot_name = "deploy_failed"
             shared.run_cmd("""\
                 dos.py suspend ${ENV_NAME} || true
@@ -79,7 +90,7 @@ def test(shared, common, steps, env_manager, jenkins_slave_node_name) {
     try {
         stage("Run tests") {
             if (env.RUN_TEST_OPTS) {
-                shared.swarm_run_pytest(steps, jenkins_slave_node_name)
+                shared.swarm_run_pytest(steps, jenkins_slave_node_name, make_snapshot_stages)
             } else {
                 common.printMsg("RUN_TEST_OPTS is empty, skipping 'swarm-run-pytest' job", "green")
             }
@@ -87,7 +98,7 @@ def test(shared, common, steps, env_manager, jenkins_slave_node_name) {
 
     } catch (e) {
         common.printMsg("Tests are failed: " + e.message, "purple")
-        if (env_manager == 'devops') {
+        if (make_snapshot_stages) {
             def snapshot_name = "tests_failed"
             shared.run_cmd("""\
                 dos.py suspend ${ENV_NAME} || true
@@ -117,7 +128,7 @@ def test(shared, common, steps, env_manager, jenkins_slave_node_name) {
         common.printMsg("Job is failed: " + e.message, "purple")
         throw e
     } finally {
-        if (env_manager == 'devops') {
+        if (make_snapshot_stages) {
             // shutdown the environment if required
             if ("${env.SHUTDOWN_ENV_ON_TEARDOWN}" == "true") {
                 shared.run_cmd("""\
@@ -131,7 +142,7 @@ def test(shared, common, steps, env_manager, jenkins_slave_node_name) {
         }
         if ("${env.REPORT_TO_TESTRAIL}" != "false") {
             stage("report results to testrail") {
-                shared.swarm_testrail_report(steps, jenkins_slave_node_name)
+                shared.swarm_testrail_report(steps)
             }
             stage("Store TestRail reports to job description") {
                 def String description = readFile("description.txt")
