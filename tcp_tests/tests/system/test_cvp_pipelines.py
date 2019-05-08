7726dedc0bec73d1ecc681b4ccb67187b869a724
Oleksii Zhurba <ozhurba@mirantis.com>
2019-05-08 17:31:19 -0500

    Add test_run_cvp_stacklight
    Change-Id: I364e7a2e96050a212f77a9c48ee64164b8ba9e1f

--- tcp_tests/tests/system/test_cvp_pipelines.py
+++ tcp_tests/tests/system/test_cvp_pipelines.py
@@ -218,3 +218,59 @@ class TestCvpPipelines(object):
 
         assert cvp_ha_smoke_result == 'SUCCESS', "{0}\n{1}".format(
             description, '\n'.join(stages))
+
+    @pytest.mark.grab_versions
+    @pytest.mark.parametrize("_", [settings.ENV_NAME])
+    @pytest.mark.run_cvp_stacklight
+    def test_run_cvp_stacklight(self, salt_actions, show_step, _):
+        """Runner for Pipeline CVP - Stacklight
+
+        Scenario:
+            1. Get CICD Jenkins access credentials from salt
+            2. Run job cvp-stacklight
+            3. Get passed stages from cvp-stacklight
+        """
+        salt = salt_actions
+        show_step(1)
+
+        tgt = 'I@docker:client:stack:jenkins and cid01*'
+        jenkins_host = salt.get_single_pillar(
+            tgt=tgt, pillar="jenkins:client:master:host")
+        jenkins_port = salt.get_single_pillar(
+            tgt=tgt, pillar="jenkins:client:master:port")
+        jenkins_url = 'http://{0}:{1}'.format(jenkins_host, jenkins_port)
+        jenkins_user = salt.get_single_pillar(
+            tgt=tgt, pillar="jenkins:client:master:username")
+        jenkins_pass = salt.get_single_pillar(
+            tgt=tgt, pillar="jenkins:client:master:password")
+        jenkins_start_timeout = 60
+        jenkins_build_timeout = 1800
+
+        job_name = 'cvp-stacklight'
+
+        show_step(2)
+        cvp_stacklight_result = run_jenkins_job.run_job(
+            host=jenkins_url,
+            username=jenkins_user,
+            password=jenkins_pass,
+            start_timeout=jenkins_start_timeout,
+            build_timeout=jenkins_build_timeout,
+            verbose=True,
+            job_name=job_name,
+            job_parameters={},
+            job_output_prefix='[cvp-stacklight/{build_number}:platform {time}]'
+        )
+
+        show_step(3)
+        (description, stages) = get_jenkins_job_stages.get_deployment_result(
+            host=jenkins_url,
+            username=jenkins_user,
+            password=jenkins_pass,
+            job_name=job_name,
+            build_number='lastBuild')
+
+        LOG.info(description)
+        LOG.info('\n'.join(stages))
+
+        assert cvp_stacklight_result == 'SUCCESS', "{0}\n{1}".format(
+            description, '\n'.join(stages))
