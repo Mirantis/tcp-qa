405f43f9d64ced8f0b9afc76a6cce57d6fdb5228
Oleksii Butenko <obutenko@mirantis.com>
2019-05-20 16:50:47 +0300

    Fix create_env_jenkins_cicd
    Close-bug: PROD-30306
    
    Change-Id: Ia31a1f86d56cadff9bfac8f05c9adf962cdb46f2

--- tcp_tests/managers/saltmanager.py
+++ tcp_tests/managers/saltmanager.py
@@ -14,6 +14,7 @@
 
 import netaddr
 import pkg_resources
+import yaml
 
 from collections import defaultdict
 
@@ -40,7 +41,7 @@ class SaltManager(ExecuteCommandsMixin):
     }
 
     def __init__(self, config, underlay, host=None, port='6969',
-                 username=None, password=None):
+                 username=None, password=None, domain_name=None):
         self.__config = config
         self.__underlay = underlay
         self.__port = port
@@ -48,6 +49,7 @@ class SaltManager(ExecuteCommandsMixin):
         self.__api = None
         self.__user = username or settings.SALT_USER
         self.__password = password or settings.SALT_PASSWORD
+        self.__domain_name = domain_name
         self._salt = self
 
         super(SaltManager, self).__init__(config=config, underlay=underlay)
@@ -371,16 +373,19 @@ class SaltManager(ExecuteCommandsMixin):
         env_jenkins_cicd_filename = pkg_resources.resource_filename(
             settings.__name__, 'utils/env_jenkins_cicd')
 
-        tgt = 'I@docker:client:stack:jenkins and cid01*'
-        try:
-            jenkins_params = self.get_single_pillar(
-                tgt=tgt, pillar="jenkins:client:master")
-        except LookupError as e:
-            LOG.error("Skipping creation {0} because cannot get Jenkins CICD "
-                      "parameters from '{1}': {2}"
-                      .format(env_jenkins_cicd_filename, tgt, e.message))
+        tgt = 'cid01.' + settings.DOMAIN_NAME
+        command = "reclass -n %".format(tgt)
+        output = self.__underlay.check_call(
+            node_name=self.__host,
+            cmd=command)
+        result = yaml.load(output)
+        jenkins_params = result.get(
+            'parameters', {}).get(
+            'jenkins', {}).get(
+            'client', {}).get(
+            'master', {})
+        if not jenkins_params:
             return
-
         jenkins_host = jenkins_params['host']
         jenkins_port = jenkins_params['port']
         jenkins_user = jenkins_params['username']
