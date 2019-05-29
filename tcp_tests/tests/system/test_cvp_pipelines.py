0f954e2dca6978ac85321da01ece108f20e9d44b
Oleksii Zhurba <legan4ik@gmail.com>
2019-05-29 16:33:05 -0500

    Add CVP-tempest
    Change-Id: Ib21b0bd5366597d443527c7581508eb7ae031821

--- tcp_tests/tests/system/test_cvp_pipelines.py
+++ tcp_tests/tests/system/test_cvp_pipelines.py
@@ -326,3 +326,91 @@ class TestCvpPipelines(object):
         except jenkins.NotFoundException:
             raise jenkins.NotFoundException("{0}\n{1}".format(
                 description, '\n'.join(stages)))
+
+    @pytest.mark.grab_versions
+    @pytest.mark.parametrize("_", [settings.ENV_NAME])
+    @pytest.mark.run_cvp_tempest
+    def test_run_cvp_tempest(self, salt_actions, show_step, _):
+        """Runner for Pipeline CVP - Tempest
+
+        Scenario:
+            1. Get CICD Jenkins access credentials from salt
+            2. Run job cvp-tempest
+            3. Get passed stages from cvp-tempest
+            4. Download XML report from the job
+        """
+        salt = salt_actions
+        show_step(1)
+
+        tgt = 'I@docker:client:stack:jenkins and cid01*'
+        jenkins_host = salt.get_single_pillar(
+            tgt=tgt, pillar="jenkins:client:master:host")
+        jenkins_port = salt.get_single_pillar(
+            tgt=tgt, pillar="jenkins:client:master:port")
+        jenkins_url = 'http://{0}:{1}'.format(jenkins_host, jenkins_port)
+        jenkins_user = salt.get_single_pillar(
+            tgt=tgt, pillar="jenkins:client:master:username")
+        jenkins_pass = salt.get_single_pillar(
+            tgt=tgt, pillar="jenkins:client:master:password")
+        jenkins_start_timeout = 60
+        jenkins_build_timeout = 1800
+
+        os_version = salt.get_single_pillar(
+            tgt='I@salt:master', pillar="_param:openstack_version")
+
+        job_name = 'cvp-tempest'
+        job_parameters = {
+            'EXTRA_PARAMS': (
+                """
+                ---
+                  DEBUG_MODE: false
+                  GENERATE_CONFIG: true
+                  TARGET_NODE: 'cid01*'
+                  TEST_IMAGE: 'docker-prod-virtual.docker.mirantis.net/mirantis/cicd/ci-tempest:{0}'
+                  report_prefix: 'cvp_'"""
+                .format(os_version)),
+        }
+
+        show_step(2)
+        cvp_tempest_result = run_jenkins_job.run_job(
+            host=jenkins_url,
+            username=jenkins_user,
+            password=jenkins_pass,
+            start_timeout=jenkins_start_timeout,
+            build_timeout=jenkins_build_timeout,
+            verbose=True,
+            job_name=job_name,
+            job_parameters=job_parameters,
+            job_output_prefix='[cvp-tempest/{build_number}:platform {time}]'
+        )
+
+        show_step(3)
+        (description, stages) = get_jenkins_job_stages.get_deployment_result(
+            host=jenkins_url,
+            username=jenkins_user,
+            password=jenkins_pass,
+            job_name=job_name,
+            build_number='lastBuild')
+
+        LOG.info(description)
+        LOG.info('\n'.join(stages))
+        LOG.info('Job {0} result: {1}'.format(job_name,
+                                              cvp_tempest_result))
+        # Download XML report
+        show_step(4)
+        destination_name = os.path.join(settings.LOGS_DIR,
+                                        "tempest_report.xml")
+        # Do not fail the test case when the job is failed, but
+        # artifact with the XML report is present in the job.
+        try:
+            get_jenkins_job_artifact.download_artifact(
+                host=jenkins_url,
+                username=jenkins_user,
+                password=jenkins_pass,
+                job_name=job_name,
+                build_number='lastBuild',
+                artifact_path='validation_artifacts/cvp-tempest_report.xml',
+                destination_name=destination_name)
+        except jenkins.NotFoundException:
+            raise jenkins.NotFoundException("{0}\n{1}".format(
+                description, '\n'.join(stages)))
