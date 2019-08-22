480ee5c2d3fa471605833963574edc8512524db5
Hanna Arhipova <annkapul@gmail.com>
2019-08-22 14:11:46 +0300

    Add scenario which enables automatic Galera upgrade
    Change-Id: I080f169292d62f12bb3fa7496e3644a655ffe131

---
+++ tcp_tests/fixtures/reclass_fixtures.py
@@ -0,0 +1,17 @@
+import pytest
+
+from tcp_tests import logger
+from tcp_tests.managers import reclass_manager
+
+LOG = logger.logger
+
+
+@pytest.fixture(scope='function')
+def reclass_actions(config, underlay):
+    """Fixture that provides various actions for salt
+
+    :param config: fixture provides oslo.config
+    :param underlay_actions: fixture provides underlay manager
+    :rtype: ReclassManager
+    """
+    return reclass_manager.ReclassManager(config, underlay)
