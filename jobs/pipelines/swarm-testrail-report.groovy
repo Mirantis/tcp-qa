0cc7691d2820c3c3dd0c7b49be32348ab7a727e8
Ekaterina Chernova <efedorova@mirantis.com>
2019-10-25 12:35:15 +0300

    Add tcp-qa configuration to testrail-reporter
    Change-Id: I7c9566ae56c45f26b7f743abbbdf22583ecf5f63

--- jobs/pipelines/swarm-testrail-report.groovy
+++ jobs/pipelines/swarm-testrail-report.groovy
@@ -105,6 +105,7 @@ node ("${PARENT_NODE_NAME}") {
                       "--testrail-add-missing-cases",
                       "--testrail-case-custom-fields {\\\"custom_qa_team\\\":\\\"9\\\"}",
                       "--testrail-case-section-name \'All\'",
+                      "--testrail_configuration_name \'tcp-qa\'",
                     ]
                     ret = shared.upload_results_to_testrail(tcpqa_report_name, testSuiteName, methodname, testrail_name_template, reporter_extra_options)
                     common.printMsg(ret.stdout, "blue")
@@ -212,6 +213,7 @@ node ("${PARENT_NODE_NAME}") {
                       "--testrail-add-missing-cases",
                       "--testrail-case-custom-fields {\\\"custom_qa_team\\\":\\\"9\\\"}",
                       "--testrail-case-section-name \'All\'",
+                      "--testrail_configuration_name \'tcp-qa\'",
                     ]
                     ret = shared.upload_results_to_testrail(cvp_sanity_report_name, testSuiteName, methodname, testrail_name_template, reporter_extra_options)
                     common.printMsg(ret.stdout, "blue")
