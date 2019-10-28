34799d238a994e5d0a5246ab4d5d4a46dfc57fdd
Kate <katyafervent@yandex.ru>
2019-10-28 14:09:24 +0000

    Revert "Add tcp-qa configuration to testrail-reporter"
    This reverts commit 26d852389963277245549386fba06e9cff4b7efd.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: I27a5e0275779cf041d2f811431c110ec24525599

--- jobs/pipelines/swarm-testrail-report.groovy
+++ jobs/pipelines/swarm-testrail-report.groovy
@@ -105,7 +105,6 @@ node ("${PARENT_NODE_NAME}") {
                       "--testrail-add-missing-cases",
                       "--testrail-case-custom-fields {\\\"custom_qa_team\\\":\\\"9\\\"}",
                       "--testrail-case-section-name \'All\'",
-                      "--testrail_configuration_name \'tcp-qa\'",
                     ]
                     ret = shared.upload_results_to_testrail(tcpqa_report_name, testSuiteName, methodname, testrail_name_template, reporter_extra_options)
                     common.printMsg(ret.stdout, "blue")
@@ -124,16 +123,7 @@ node ("${PARENT_NODE_NAME}") {
                     testSuiteName = env.TEMPEST_TEST_SUITE_NAME
                     methodname = "{classname}.{methodname}"
                     testrail_name_template = "{title}"
-                    reporter_extra_options = [
-                      "--send-duplicates",
-                      "--testrail-add-missing-cases",
-                      "--testrail_configuration_name \'tcp-qa\'",
-                    ]
-                    ret = shared.upload_results_to_testrail(tempest_report_name,
-                                                            testSuiteName,
-                                                            methodname,
-                                                            testrail_name_template,
-                                                            reporter_extra_options)
+                    ret = shared.upload_results_to_testrail(tempest_report_name, testSuiteName, methodname, testrail_name_template)
                     common.printMsg(ret.stdout, "blue")
                     report_url = ret.stdout.split("\n").each {
                         if (it.contains("[TestRun URL]")) {
@@ -222,7 +212,6 @@ node ("${PARENT_NODE_NAME}") {
                       "--testrail-add-missing-cases",
                       "--testrail-case-custom-fields {\\\"custom_qa_team\\\":\\\"9\\\"}",
                       "--testrail-case-section-name \'All\'",
-                      "--testrail_configuration_name \'tcp-qa\'",
                     ]
                     ret = shared.upload_results_to_testrail(cvp_sanity_report_name, testSuiteName, methodname, testrail_name_template, reporter_extra_options)
                     common.printMsg(ret.stdout, "blue")
